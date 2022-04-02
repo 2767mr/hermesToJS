9b765cf59314e71a7db522036ad93a0797fa3d9d
2767mr <2767mr@gmx.at>
2022-04-02 12:52:56 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import fs from 'fs';
 import { Decompiler } from './decomp';
 import { Disassembler } from './disasm';
 import { Parser } from './parser';
+import { Verify } from './verify';
 
-const data = fs.readFileSync('../hermes/functions.bundle');
+
+const data = fs.readFileSync('../hermes/objects.bundle');
 const parser = new Parser(data);
 const header = parser.parse();
 
 const disasm = new Disassembler(data, header);
 const decomp = new Decompiler(disasm, header);
-decomp.decompile();
+const result = decomp.decompile('result.js');
+
+const verify = new Verify();
+verify.verify(result, data);
+
+console.log(result);
+fs.writeFileSync('result.js', result);
