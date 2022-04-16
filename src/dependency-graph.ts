1b5a7eed1581f4b931c14e8e4cbe0eded14c1305
2767mr <2767mr@gmx.at>
2022-04-16 23:27:41 +0200

    Added some optimizer steps back

--- src/dependency-graph.ts
+++ src/dependency-graph.ts
@@ -7,7 +7,7 @@ function debug(node: ts.Node, file: ts.SourceFile) {
 export interface DependencyNode {
 	code: string;
 	node: ts.Statement;
-	dependsOn: DependencyNode[][];
+	dependsOn: Record<string, DependencyNode[]>;
 	affects: DependencyNode[];
 }
 
@@ -18,31 +18,34 @@ export class DependencyGraph {
 		this.analyse(file, new Map());
 
 		for (const [_, node] of this.nodes) {
-			console.log(debug(node.node, file));
-			console.log('  affects: ' + node.affects.map(a => debug(a.node, file)).join(' | '));
-			console.log('  depends: ' + node.dependsOn.flatMap(ds => ds.map(d => debug(d.node, file))).join(' | '));
+			console.log(debug(node.node, file).split('\n')[0]);
+			console.log('  affects: ' + node.affects.map(a => debug(a.node, file).split('\n')[0]).join(' | '));
+			console.log('  depends:');
+			for (const [d, val] of Object.entries(node.dependsOn)) {
+				console.log('    ' + d + ': ' + val.map(a => debug(a.node, file).split('\n')[0]).join(' | '));
+			}
 		}
 	}
 
 	private analyse(block: ts.SourceFile | ts.Block, identifiers: Map<ts.Identifier, DependencyNode[]>) {
-		const markDependency = (node: ts.Node, newNode: DependencyNode) => {
-			this.nodeAndForEachDescendant(node, n => {
+		const markDependency = (node: ts.Node, newNode: DependencyNode, prefix = '') => {
+			this.nodeAndForEachDescendant(node, (n, path) => {
 				if (ts.isIdentifier(n) && identifiers.has(n) && 'autoGenerateId' in n) {
 					for (const parent of identifiers.get(n)!) {
-						if (!parent.affects.includes(newNode)) {
+						if (parent.affects.every(a => a.node !== newNode.node)) {
 							parent.affects.push(newNode);
 						}
 					}
-					newNode.dependsOn.push(identifiers.get(n)!);
+					newNode.dependsOn[path] = [...identifiers.get(n)!];
 				}
-			});
+			}, prefix);
 		};
 
 		const mergeDependencies = (oldIds: Map<ts.Identifier, DependencyNode[]>) => {
 			for (const [id, nodes] of oldIds) {
-				const newNodes = identifiers.get(id) ?? [];
+				const newNodes = [...identifiers.get(id) ?? []];
 				for (const node of nodes) {
-					if (!newNodes.includes(node)) {
+					if (newNodes.every(n => n.node !== node.node)) {
 						newNodes.push(node);
 					}
 				}
@@ -51,46 +54,50 @@ export class DependencyGraph {
 		};
 
 		for (const stmt of block.statements) {
-			const newNode: DependencyNode = {
+			const newNode: DependencyNode = this.nodes.get(stmt) ?? {
 				code: debug(stmt, this.file),
 				node: stmt,
 				affects: [],
-				dependsOn: [],
+				dependsOn: {},
 			};
 
 			if (ts.isExpressionStatement(stmt)
 			&& ts.isBinaryExpression(stmt.expression)
 			&& stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment) {
-				markDependency(stmt.expression.right, newNode);
+				markDependency(stmt.expression.right, newNode, '.expression.right');
 
 				const id = stmt.expression.left as ts.Identifier;
 				identifiers.set(id, [newNode]);
 			} else if (ts.isIfStatement(stmt)) {
-				markDependency(stmt.expression, newNode);
+				markDependency(stmt.expression, newNode, '.expression');
 
 				const oldIds = new Map(identifiers);
 				this.analyse(stmt.thenStatement as ts.Block, identifiers);
 				if (stmt.elseStatement) {
-					this.analyse(stmt.thenStatement as ts.Block, oldIds);
+					this.analyse(stmt.elseStatement as ts.Block, oldIds);
 				}
 				mergeDependencies(oldIds);
 			} else if (ts.isWhileStatement(stmt)) {
-				markDependency(stmt.expression, newNode);
+				markDependency(stmt.expression, newNode, '.expression');
 
 				const oldIds = new Map(identifiers);
 				this.analyse(stmt.statement as ts.Block, identifiers);
 				mergeDependencies(oldIds);
 
-				markDependency(stmt.expression, newNode);
+				const oldIds2 = new Map(identifiers);
+				this.analyse(stmt.statement as ts.Block, identifiers);
+				mergeDependencies(oldIds2);
+
+				markDependency(stmt.expression, newNode, '.expression');
 			} else if (ts.isDoStatement(stmt)) {
 				this.analyse(stmt.statement as ts.Block, identifiers);
-				markDependency(stmt.expression, newNode);
+				markDependency(stmt.expression, newNode, '.expression');
 
 				const oldIds = new Map(identifiers);
 				this.analyse(stmt.statement as ts.Block, identifiers);
 				mergeDependencies(oldIds);
 				
-				markDependency(stmt.expression, newNode);
+				markDependency(stmt.expression, newNode, '.expression');
 			} else if (ts.isVariableStatement(stmt)) {
 				for (const v of stmt.declarationList.declarations) {
 					identifiers.set(v.name as ts.Identifier, [newNode]);
@@ -104,8 +111,59 @@ export class DependencyGraph {
 			
 	}
 
-	private nodeAndForEachDescendant(node: ts.Node, visitor: (node: ts.Node) => void) {
-		visitor(node);
-		ts.forEachChild(node, (n) => this.nodeAndForEachDescendant(n, visitor));
+	private nodeAndForEachDescendant(node: ts.Node, visitor: (node: ts.Node, path: string) => void, path = '') {
+		visitor(node, path);
+		
+		const visitCount = new Map<ts.Node | ts.NodeArray<ts.Node>, number>();
+		
+		ts.forEachChild(
+			node, 
+			(n) => this.nodeAndForEachDescendant(n, visitor, getPath(n)),
+			nodes => {
+				const base = getPath(nodes) + '.';
+				for (let i = 0; i < nodes.length; i++) {
+					this.nodeAndForEachDescendant(nodes[i], visitor, base + i);
+				}
+			});
+
+		function getPath(child: ts.Node | ts.NodeArray<ts.Node>) {
+			const paths = Object.keys(node).filter((key) => node[key as keyof ts.Node] === child);
+			if (paths.length === 0) {
+				throw new Error('could not resolve path');
+			}
+			let index = 0;
+			if (paths.length > 1) {
+				index = visitCount.get(child) ?? 0;
+				visitCount.set(child, index + 1);
+			}
+			return path + '.' + paths[index];
+		}
+	}
+
+	public inlineNode(from: ts.Node, to: ts.Node, path: string): ts.Node {
+		this.assign(to, path, from);
+		return to;
+	}
+
+	public replace(from: ts.Statement, to: ts.Statement, dependsOn?: Record<string, DependencyNode[]>, affects?: DependencyNode[]) {
+		const node = this.nodes.get(from)!;
+		node.node = to;
+		node.dependsOn = dependsOn ?? node.dependsOn;
+		node.affects = affects ?? node.affects;
+		this.nodes.set(to, node);
+	}
+
+	private assign(obj: unknown, path: string, value: unknown) {
+		let lastObj = obj;
+		let lastPath = path;
+		while (path.length > 0) {
+			lastObj = obj;
+			lastPath = path;
+			const nextDot = path.indexOf('.', 1);
+			const nextPath = path.substring(1, nextDot === -1 ? undefined : nextDot);
+			obj = (obj as Record<string, unknown>)[nextPath];
+			path = nextDot === -1 ? '' : path.substring(nextDot);
+		}
+		(lastObj as Record<string, unknown>)[lastPath.substring(1)] = value;
 	}
 }
--- src/optimizer.ts
+++ src/optimizer.ts
@@ -1,23 +1,19 @@
 import ts from 'typescript';
 import { DependencyGraph } from './dependency-graph';
 
-function debug(node: ts.Node) {
-	return ts.createPrinter().printNode(ts.EmitHint.Unspecified, node, {} as ts.SourceFile);
-}
 
 export class Optimizer {
 	public optimize(file: ts.SourceFile) {		
 		ts.createPrinter().printFile(file);
 
 		file = this.inlineGlobal(file);
-		new DependencyGraph(file);
-		file = this.inlineSimpleLiteral(file);
-		file = this.inlineComplexLiteral(file);
-		file = this.writeAfterWrite(file);
-		file = this.deleteDanglingLiteral(file);
+		const graph = new DependencyGraph(file);
+		file = this.inlineLiteral(file, graph);
+		file = this.removeWAW(file, graph);
+		file = this.inlinePreceding(file, graph);
 		return file;
 	}
-
+	
 	//Assumes that globalThis has it's own reserved variable and removes it
 	private inlineGlobal(file: ts.SourceFile) {
 		const globalRegs = new Set<ts.Identifier>();
@@ -58,212 +54,36 @@ export class Optimizer {
 		});
 	}
 
-	//Removes unnecessary assignments
-	private writeAfterWrite(file: ts.SourceFile) {
-		const [waw] = this.analyseWAWBlock(new Set(), new Map(), file);
-		return this.transform(file, (node, visit, ctx) => {
-			if (ts.isBlock(node)) {
-				const stmts = Array.from(node.statements);
-				for (let i = 0; i < stmts.length; i++) {
-					const stmt = stmts[i];
-					if (ts.isExpressionStatement(stmt)
-                    && ts.isBinaryExpression(stmt.expression)
-                    && stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
-                    && waw.has(stmt)) {
-						stmts[i] = ts.factory.createExpressionStatement(stmt.expression.right);
-					}
-				}
-				return ts.visitEachChild(ts.factory.createBlock(stmts), visit, ctx);
-			} else if (ts.isSourceFile(node)) {
-				const stmts = Array.from(node.statements);
-				for (let i = 0; i < stmts.length; i++) {
-					const stmt = stmts[i];
-					if (ts.isExpressionStatement(stmt)
-                    && ts.isBinaryExpression(stmt.expression)
-                    && stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
-                    && waw.has(stmt)) {
-						stmts[i] = ts.factory.createExpressionStatement(stmt.expression.right);
-					}
-				}
-
-				node = ts.factory.updateSourceFile(node, stmts);
-				return ts.visitEachChild(node, visit, ctx);
-			}
-			return ts.visitEachChild(node, visit, ctx);
-		});
-	}
-	private analyseWAWBlock(waw: Set<ts.Statement>, written: Map<ts.Identifier, ts.Statement[]>, block: ts.Block | ts.SourceFile) {
-		const resultWAW = new Set(waw);
-		const resultWritten = new Map(written);
-
-		function merge(
-			thenWAW: Set<ts.Statement>, thenWritten: Map<ts.Identifier, ts.Statement[]>,
-			elseWAW: Set<ts.Statement>, elseWritten: Map<ts.Identifier, ts.Statement[]>
-		) {
-			const writtenStmts = new Set(Array.from(resultWritten.values()).flat());
-			for (const waw of thenWAW) {
-				if (elseWAW.has(waw) || !writtenStmts.has(waw)) {
-					resultWAW.add(waw);
-				}
-			}
-
-			const thenWrittenStmts = new Set<ts.Statement>();
-			const elseWrittenStmts = new Set<ts.Statement>();
-			
-			resultWritten.clear();
-			for (const [id, stmts] of thenWritten) {
-				resultWritten.set(id, stmts);
-				for (const stmt of stmts) {
-					if (writtenStmts.has(stmt)) {
-						thenWrittenStmts.add(stmt);
-					}
-				}
-			}
-			
-			for (const [id, stmts] of elseWritten) {
-				for (const stmt of stmts) {
-					if (writtenStmts.has(stmt)) {
-						elseWrittenStmts.add(stmt);
-					}
-				}
-
-				if (resultWritten.has(id)) {
-					resultWritten.get(id)!.push(...stmts);
-				} else {
-					resultWritten.set(id, stmts);
-				}
-			}
-
-			for (const [, stmts] of resultWritten) {
-				for (const stmt of stmts) {
-					if (writtenStmts.has(stmt) && (!thenWrittenStmts.has(stmt) || !elseWrittenStmts.has(stmt))) {
-						while (stmts.includes(stmt)) {
-							stmts.splice(stmts.indexOf(stmt), 1);
-						}
-					}
-				}
-			}
-		}
-		
-		for (const stmt of block.statements) {
-			if (ts.isExpressionStatement(stmt)
-			&& ts.isBinaryExpression(stmt.expression)
-			&& stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment) {
-				this.nodeAndForEachDescendant(stmt.expression.right, node => {
-					if (ts.isIdentifier(node)) {
-						resultWritten.delete(node);
-					}
-				});
-
-				const id = stmt.expression.left as ts.Identifier;
-				const written = resultWritten.get(id);
-				if (written) {
-					for (const w of written) {
-						resultWAW.add(w);
-					}
-				}
-				resultWritten.set(id, [stmt]);
-			} else if (ts.isIfStatement(stmt)) {
-				this.nodeAndForEachDescendant(stmt.expression, node => {
-					if (ts.isIdentifier(node)) {
-						resultWritten.delete(node);
-					}
-				});
-
-				const [thenWAW, thenWritten] = this.analyseWAWBlock(resultWAW, resultWritten, stmt.thenStatement as ts.Block);
-				const [elseWAW, elseWritten] = stmt.elseStatement 
-					? this.analyseWAWBlock(resultWAW, resultWritten, stmt.elseStatement as ts.Block)
-					: [resultWAW, new Map(resultWritten)];
-				merge(thenWAW, thenWritten, elseWAW, elseWritten);
-			} else if (ts.isWhileStatement(stmt)) {
-				this.nodeAndForEachDescendant(stmt.expression, node => {
-					if (ts.isIdentifier(node)) {
-						resultWritten.delete(node);
-					}
-				});
-
-				const [thenWAW, thenWritten] = this.analyseWAWBlock(resultWAW, resultWritten, stmt.statement as ts.Block);
-				const [elseWAW, elseWritten] = [resultWAW, new Map(resultWritten)];
-				merge(thenWAW, thenWritten, elseWAW, elseWritten);
-				
-				this.nodeAndForEachDescendant(stmt.expression, node => {
-					if (ts.isIdentifier(node)) {
-						resultWritten.delete(node);
-					}
-				});
-
-			} else if (ts.isDoStatement(stmt)) {
-				const [doWAW, doWritten] = this.analyseWAWBlock(resultWAW, resultWritten, stmt.statement as ts.Block);
-				resultWAW.clear();
-				for (const w of doWAW) {
-					resultWAW.add(w);
-				}
-				resultWritten.clear();
-				for (const [id, w] of doWritten) {
-					resultWritten.set(id, w);
-				}
-
-				this.nodeAndForEachDescendant(stmt.expression, node => {
-					if (ts.isIdentifier(node)) {
-						resultWritten.delete(node);
-					}
-				});
-				
-				const [thenWAW, thenWritten] = this.analyseWAWBlock(resultWAW, resultWritten, stmt.statement as ts.Block);
-				const [elseWAW, elseWritten] = [resultWAW, new Map(resultWritten)];
-				merge(thenWAW, thenWritten, elseWAW, elseWritten);
-				
-				this.nodeAndForEachDescendant(stmt.expression, node => {
-					if (ts.isIdentifier(node)) {
-						resultWritten.delete(node);
-					}
-				});
-			} else {
-				this.nodeAndForEachDescendant(stmt, node => {
-					if (ts.isIdentifier(node)) {
-						resultWritten.delete(node);
-					}
-				});
-
-			}
-		}
-			
-
-		return [resultWAW, resultWritten] as const;
-	}
-
+	
 	//Inlines literals that have reserved variables
-	private inlineSimpleLiteral(file: ts.SourceFile) {
-		const literals = new Map<ts.Identifier, ts.NumericLiteral | ts.StringLiteral>();
-		const complexRegisters = new Set<ts.Identifier>();
-
-		this.nodeAndForEachDescendant(file, (stmt) => {
-			if (ts.isExpressionStatement(stmt)
-			&& ts.isBinaryExpression(stmt.expression)
-			&& stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
-			&& ts.isIdentifier(stmt.expression.left)) {
-				const id = stmt.expression.left;
-				if (!complexRegisters.has(id) && (ts.isStringLiteral(stmt.expression.right) || ts.isStringLiteral(stmt.expression.right))) {
-					literals.set(id, stmt.expression.right);
-				} else {
-					complexRegisters.add(id);
-					literals.delete(id);
-				}
-			}
-		});
-
+	private inlineLiteral(file: ts.SourceFile, graph: DependencyGraph) {
 		return this.transform(file, (node, visit, ctx) => {
 			if (ts.isBlock(node) || ts.isSourceFile(node)) {
 				const stmts = Array.from(node.statements);
 				for (let i = 0; i < stmts.length; i++) {
 					const stmt = stmts[i];
+					const code = this.debug(stmt);
 					if (ts.isExpressionStatement(stmt)
                     && ts.isBinaryExpression(stmt.expression)
                     && stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
 					&& ts.isIdentifier(stmt.expression.left)
-					&& literals.has(stmt.expression.left)) {
-						stmts.splice(i, 1);
-						i--;
+					&& (ts.isStringLiteral(stmt.expression.right) || ts.isNumericLiteral(stmt.expression.right))) {
+						const from = graph.nodes.get(stmt)!;
+						if (!from) {
+							console.warn('Stmt not in nodes: ' + code);
+							continue;
+						}
+						for (const to of from.affects) {
+							const [path] = Object.entries(to.dependsOn).find(([, val]) => val.includes(from))!;
+							if (to.dependsOn[path].length === 1) {
+								graph.inlineNode(stmt.expression.right, to.node, path);
+								from.affects.splice(from.affects.indexOf(to), 1);
+							}
+						}
+						if (from.affects.length === 0) {
+							stmts.splice(i, 1);
+							i--;
+						}
 					}
 				}
 
@@ -274,95 +94,31 @@ export class Optimizer {
 				}
 			}
 
-			if (ts.isIdentifier(node) && literals.has(node)) {
-				return literals.get(node)!;
-			}
-
-			if (ts.isVariableDeclarationList(node)) {
-				node = ts.factory.updateVariableDeclarationList(node, node.declarations.filter(d => !literals.has(d.name as ts.Identifier)));
-				return node;
-			}
-
 			return ts.visitEachChild(node, visit, ctx);
 		});
 	}
-
-	//Inlines literals that do not have reserved variables
-	private inlineComplexLiteral(file: ts.SourceFile) {
-		const literals = new Map<ts.Identifier, ts.NumericLiteral | ts.StringLiteral>();
-		return this.transform(file, (node, visit, ctx) => {
-			if (ts.isBlock(node) || ts.isSourceFile(node)) {
-				return this.inlineComplexLiteralBlock(node, visit, ctx, literals);
-			}
-
-			if (ts.isIdentifier(node) && literals.has(node)) {
-				return literals.get(node)!;
-			}
-
-			return ts.visitEachChild(node, visit, ctx);
-		});
-	}
-	private inlineComplexLiteralBlock(node: ts.Block | ts.SourceFile, visit: (node: ts.Node) => ts.Node, ctx: ts.TransformationContext, literals: Map<ts.Identifier, ts.NumericLiteral | ts.StringLiteral>) {
-		const blockScope = new Set<ts.Identifier>();
-		
-		const stmts = Array.from(node.statements);
-		for (let i = 0; i < stmts.length; i++) {
-			const stmt = stmts[i];
-			const stmtText = this.debug(stmt);
-			if (ts.isExpressionStatement(stmt)
-                    && ts.isBinaryExpression(stmt.expression)
-                    && stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
-					&& ts.isIdentifier(stmt.expression.left)) {
-				if (ts.isStringLiteral(stmt.expression.right) || ts.isNumericLiteral(stmt.expression.right)) {
-					literals.set(stmt.expression.left, stmt.expression.right);
-					blockScope.add(stmt.expression.left);
-				} else {
-					literals.delete(stmt.expression.left);
-					stmts[i] = ts.factory.createExpressionStatement(
-						ts.factory.updateBinaryExpression(
-							stmt.expression, 
-							stmt.expression.left, 
-							stmt.expression.operatorToken, 
-							ts.visitEachChild(stmt.expression.right, visit, ctx)
-						));
-				}
-			} else {
-				this.nodeAndForEachDescendant(stmt, n => {
-					if (ts.isExpressionStatement(n)
-                    && ts.isBinaryExpression(n.expression)
-                    && n.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
-					&& ts.isIdentifier(n.expression.left)
-					&& !ts.isStringLiteral(n.expression.right)
-					&& !ts.isNumericLiteral(n.expression.right)) {
-						literals.delete(n.expression.left);
-					}
-				});
-				stmts[i] = ts.visitEachChild(stmt, visit, ctx);
-			}
-		}
-
-		for (const id of blockScope) {
-			literals.delete(id);
-		}
-
-		if (ts.isBlock(node)) {
-			node = ts.factory.updateBlock(node, stmts);
-		} else {
-			node = ts.factory.updateSourceFile(node, stmts);
-		}
-		return node;
-	}
-
-	private deleteDanglingLiteral(file: ts.SourceFile) {
+	
+	private removeWAW(file: ts.SourceFile, graph: DependencyGraph) {
 		return this.transform(file, (node, visit, ctx) => {
 			if (ts.isBlock(node) || ts.isSourceFile(node)) {
 				const stmts = Array.from(node.statements);
 				for (let i = 0; i < stmts.length; i++) {
 					const stmt = stmts[i];
+					const code = this.debug(stmt);
 					if (ts.isExpressionStatement(stmt)
-                    && (ts.isStringLiteral(stmt.expression) || ts.isNumericLiteral(stmt.expression))) {
-						stmts.splice(i, 1);
-						i--;
+                    && ts.isBinaryExpression(stmt.expression)
+                    && stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
+					&& ts.isIdentifier(stmt.expression.left)) {
+						const from = graph.nodes.get(stmt)!;
+						if (!from) {
+							console.warn('Stmt not in nodes: ' + code);
+							continue;
+						}
+						if (from.affects.length === 0) {
+							const replaced = ts.factory.createExpressionStatement(stmt.expression.right);
+							stmts[i] = replaced;
+							graph.replace(stmt, replaced);
+						}
 					}
 				}
 
@@ -372,6 +128,99 @@ export class Optimizer {
 					node = ts.factory.updateSourceFile(node, stmts);
 				}
 			}
+
+			return ts.visitEachChild(node, visit, ctx);
+		});
+	}
+
+	private inlinePreceding(file: ts.SourceFile, graph: DependencyGraph) {
+		function getLastEvaluationDepenency(node: ts.Node): ts.Identifier | undefined {
+			if (ts.isIdentifier(node)) {
+				return 'autoGenerateId' in node ? node : undefined;
+			}
+			if (ts.isExpressionStatement(node)) {
+				return getLastEvaluationDepenency(node.expression);
+			}
+			if (ts.isBinaryExpression(node)) {
+				if (node.operatorToken.kind === ts.SyntaxKind.FirstAssignment) {
+					if (!ts.isIdentifier(node.left)) {
+						const result = getLastEvaluationDepenency(node.left);
+						if (result) {
+							return result;
+						}
+					}
+					return getLastEvaluationDepenency(node.right);
+				} else {
+					const result = getLastEvaluationDepenency(node.right);
+					if (result) {
+						return result;
+					}
+					return getLastEvaluationDepenency(node.left);
+				}
+			}
+			if (ts.isPropertyAccessChain(node)) {
+				const result = getLastEvaluationDepenency(node.expression);
+				if (result) {
+					return result;
+				}
+				return getLastEvaluationDepenency(node.name);
+			}
+			if (ts.isCallExpression(node)) {
+				for (let i = node.arguments.length - 1; i >= 0; i--) {
+					const result = getLastEvaluationDepenency(node.arguments[i]);
+					if (result) {
+						return result;
+					}
+				}
+				return getLastEvaluationDepenency(node.expression);
+			}
+		}
+
+
+		return this.transform(file, (node, visit, ctx) => {
+			if (ts.isBlock(node) || ts.isSourceFile(node)) {
+				const stmts = Array.from(node.statements);
+				for (let i = 0; i < stmts.length; i++) {
+					const stmt = stmts[i];
+					const lastStmt = stmts[i - 1];
+					const code = this.debug(stmt);
+
+					if (Object.values(graph.nodes.get(stmt)?.dependsOn ?? {}).every(d => d.length == 0)) {
+						continue;
+					}
+					const lastDep = getLastEvaluationDepenency(stmt);
+					if (lastDep) {
+						if (lastStmt 
+							&& ts.isExpressionStatement(lastStmt)
+							&& ts.isBinaryExpression(lastStmt.expression)
+							&& lastStmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
+							&& ts.isIdentifier(lastStmt.expression.left)
+							&& lastStmt.expression.left === lastDep) {
+							continue;
+						}
+					}
+
+					if (ts.isExpressionStatement(stmt)
+                    && ts.isBinaryExpression(stmt.expression)
+                    && stmt.expression.operatorToken.kind === ts.SyntaxKind.FirstAssignment
+					&& ts.isIdentifier(stmt.expression.left)) {
+						// const from = graph.nodes.get(stmt)!;
+						
+						// if (from.affects.length === 0) {
+						// 	const replaced = ts.factory.createExpressionStatement(stmt.expression.right);
+						// 	stmts[i] = replaced;
+						// 	graph.replace(stmt, replaced);
+						// }
+					}
+				}
+
+				if (ts.isBlock(node)) {
+					node = ts.factory.updateBlock(node, stmts);
+				} else {
+					node = ts.factory.updateSourceFile(node, stmts);
+				}
+			}
+
 			return ts.visitEachChild(node, visit, ctx);
 		});
 	}
